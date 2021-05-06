30318f037578caab0edb310189d3f18b9eb29a9b
Matt Gross <mbgross111@gmail.com>
2021-05-06 11:34:53 -0700

    add mutations for logging in and adding users, thoughts, reactions and friends

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models')
+const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth')
 
 const resolvers = {
@@ -10,10 +10,10 @@ const resolvers = {
           .select('-__v -password')
           .populate('thoughts')
           .populate('friends');
-    
+
         return userData;
       }
-    
+
       throw new AuthenticationError('Not logged in');
     },
     thoughts: async (parent, { username }) => {
@@ -57,7 +57,46 @@ const resolvers = {
 
       const token = signToken(user);
       return { token, user };
-    }
+    },
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({ ...args, username: context.user.username });
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+        return thought;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedThought = await Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          { $push: { reactions: { reactionBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedThought;
+      }
+      
+      throw new AuthenticationError('Not logged in')
+    },
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $push: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+
+        return updatedUser;
+      }
+      
+      throw new AuthenticationError('Not logged in')
+    },
+
   }
 };
 
